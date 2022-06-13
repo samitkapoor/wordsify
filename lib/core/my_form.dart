fa779d3b4e425c98e6d5e38ae7fb6444f83f3b3c
samitkapoor <samitkapoor77@gmail.com>
2022-06-13 15:00:00 +0530

    Added TextFormField

--- lib/core/my_form.dart
+++ lib/core/my_form.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 class MyForm extends StatelessWidget {
   MyForm({super.key});
 
-  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(15),
-    borderSide: const BorderSide(
-      color: Colors.white,
-    ),
-  );
+  OutlineInputBorder outlineInputBorder({required Color color}) =>
+      OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15),
+        borderSide: BorderSide(
+          color: color,
+          width: 5,
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +34,32 @@ class MyForm extends StatelessWidget {
               children: [
                 TextFormField(
                   decoration: InputDecoration(
-                    border: outlineInputBorder,
-                    errorBorder: outlineInputBorder,
-                    enabledBorder: outlineInputBorder,
-                    focusedBorder: outlineInputBorder,
-                    disabledBorder: outlineInputBorder,
-                    focusedErrorBorder: outlineInputBorder,
+                    contentPadding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                    border: outlineInputBorder(color: Colors.white),
+                    errorBorder: outlineInputBorder(color: Colors.white),
+                    enabledBorder: outlineInputBorder(color: Colors.white),
+                    focusedBorder: outlineInputBorder(
+                        color: Theme.of(context).appBarTheme.backgroundColor
+                            as Color),
+                    disabledBorder: outlineInputBorder(color: Colors.white),
+                    focusedErrorBorder: outlineInputBorder(color: Colors.white),
                   ),
+                  cursorHeight: 25,
+                  cursorColor: Colors.white,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4!
+                      .copyWith(fontSize: 25),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  cursorWidth: 2,
+                  keyboardType: TextInputType.text,
+                  onChanged: (val) {},
+                  validator: (query) {
+                    if (query!.isEmpty) {
+                      return "This field cannot be empty";
+                    }
+                    return null;
+                  },
                 ),
               ],
             ),
